6a8815d8b88ce8c5807eb76a13a214e4303f99b0
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-12-14 21:43:40 -0500

    installed mysql2, fixed bugs to start server

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers'); // Import route handlers
-const helpers = require('./utils/helpers'); // Import custom Handlebars helpers
+// const helpers = require('./utils/helpers'); // Import custom Handlebars helpers
 
 // Create an instance of the Express application
 const PORT = process.env.PORT || 3001; // Define the port to listen on
 const app = express();
 
 // Create an instance of Handlebars with custom helpers
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 // Connect to the database using Sequelize
 const sequelize = require('./config/connection');
@@ -29,21 +29,21 @@ const sess = {
 };
 
 // Set up session middleware
-app.use(session(sess));
+// app.use(session(sess));
 
 // Configure Express to use Handlebars as the template engine
-app.engine('handlebars', hbs.engine);
+// app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // Set up middleware for parsing JSON and urlencoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// // Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the defined routes
-app.use(routes);
+// // Use the defined routes
+// app.use(routes);
 
 // Sync the database and start the server
 sequelize.sync({ force: false }).then(() => {
